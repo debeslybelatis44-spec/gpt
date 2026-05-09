38da5e909a169fcd2a6f990f6a3dab85dd0ab4a3
debeslybelatis44-spec <debeslybelatis44@gmail.com>
2026-05-08 22:15:27 -0400

    Update MainActivity.kt

--- app/src/main/java/com/lanovatechnologie/sunmiprinter/MainActivity.kt
+++ app/src/main/java/com/lanovatechnologie/sunmiprinter/MainActivity.kt
@@ -1,1 +1,46 @@
-app/src/main/java/com/lanovatechnologie/sunmiprinter/MainActivity.kt
+package com.lanovatechnologie.sunmiprinter
+
+import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.webView)
+
+        val webSettings: WebSettings = webView.settings
+        webSettings.javaScriptEnabled = true
+        webSettings.domStorageEnabled = true
+        webSettings.setSupportZoom(false)
+        webSettings.allowFileAccess = true
+        webSettings.allowContentAccess = true
+
+        webView.addJavascriptInterface(WebAppInterface(this), "Android")
+        webView.loadUrl("https://lotato1.onrender.com")
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                url?.let { view?.loadUrl(it) }
+                return true
+            }
+        }
+
+        webView.webChromeClient = WebChromeClient()
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
