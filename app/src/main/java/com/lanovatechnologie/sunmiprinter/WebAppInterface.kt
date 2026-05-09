8e283d88c514095809b2537c1a8c58fafffe7e62
debeslybelatis44-spec <debeslybelatis44@gmail.com>
2026-05-08 22:16:23 -0400

    Update WebAppInterface.kt

--- app/src/main/java/com/lanovatechnologie/sunmiprinter/WebAppInterface.kt
+++ app/src/main/java/com/lanovatechnologie/sunmiprinter/WebAppInterface.kt
@@ -1,1 +1,28 @@
+package com.lanovatechnologie.sunmiprinter
 
+import android.content.Context
+import android.webkit.JavascriptInterface
+
+class WebAppInterface(private val context: Context) {
+    private val printerManager = SunmiPrinterManager(context)
+
+    @JavascriptInterface
+    fun printTicket(ticketData: String) {
+        printerManager.printText(ticketData)
+    }
+
+    @JavascriptInterface
+    fun printImage(base64Image: String) {
+        printerManager.printImage(base64Image)
+    }
+
+    @JavascriptInterface
+    fun printBarcode(barcodeData: String) {
+        printerManager.printBarcode(barcodeData)
+    }
+
+    @JavascriptInterface
+    fun printQRCode(qrData: String) {
+        printerManager.printQRCode(qrData)
+    }
+}
