6e4a9989bd0ded5c403ac2903e9139815973f377
debeslybelatis44-spec <debeslybelatis44@gmail.com>
2026-05-10 23:20:09 -0400

    Add WebView to MainActivity for URL loading
    Implemented a WebView to load a URL and handle back navigation.

--- app/src/main/java/com/lotato/app/MainActivity.java
+++ app/src/main/java/com/lotato/app/MainActivity.java
@@ -1,1 +1,44 @@
+package com.lotato.app;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.webkit.WebChromeClient;
+
+public class MainActivity extends Activity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setLoadWithOverviewMode(true);
+        settings.setUseWideViewPort(true);
+        settings.setBuiltInZoomControls(false);
+        settings.setDatabaseEnabled(true);
+        settings.setAllowFileAccess(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient());
+
+        webView.loadUrl("https://lotato1.onrender.com/agent1.html");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
